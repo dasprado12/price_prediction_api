cf7b2b94041a9ff19a11e8803547430e682b6091
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-03-24 15:58:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="Exposes prection data",
     author="Daniel Prado, Pedro Negrão, Mateus Leite",
     author_email="danielsprado12@gmail.com, pedron_1997@hotmail.com, mateuspedrosa38@gmail.com",
-    install_requires=['flask', 'requests', 'uwsgi', 'tensorflow', 'keras', 'pandas', 'numpy'],
+    install_requires=['flask', 'requests', 'uwsgi', 'tensorflow-cpu', 'keras', 'pandas', 'numpy'],
     python_requires='>=3.6',
 )
