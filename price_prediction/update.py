a42dbaca001c62f379fe68670fa94ee7332d1085
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-05-10 11:24:49 -0300

    busday_count implementation

--- price_prediction/update.py
+++ price_prediction/update.py
@@ -1,6 +1,8 @@
 import datetime 
+from datetime import date
 from dateutil.parser import parse
 import pandas as pd
+import numpy as np
 
 class UpdateDataFrame:
 
@@ -13,24 +15,24 @@ class UpdateDataFrame:
         # Checar a ultima data .tail(1)
         #   se a ultima data for a de hoje, retorna o dataframe
         #   se não for, passa para DateUpdate
-        
         # Pega o dia de hoje
-        today_str = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)
-        today = datetime.datetime.now()
+        today_str = date.today().strftime("%Y-%m-%d")
         df = pd.read_csv('./price_prediction/Data/{}.csv'.format(self.data_name))
 
         # Pega o ultimo dia do dataframe
         df_last_day = df["Date"].iloc[-1]
-        df_last_day = datetime.datetime.strptime(df_last_day, '%Y-%m-%d')
+        # df_last_day = datetime.datetime.strptime(df_last_day, '%Y-%m-%d')
 
-        if df_last_day == today:
+        if df_last_day == today_str:
             return df
         else:
-            num_days = today - df_last_day
-            return self.DateUpdate(df, num_days.days)
+
+            num_days = np.busday_count(df_last_day, today_str)
+            return self.DateUpdate(df, num_days)
         
 
     def DateUpdate(self, df, days):
+        print(days)
         # calcular o número de requisições a serem feitas
         # Faz as requisições em um loop for
         # Atualiza o datadrame
